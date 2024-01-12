16913c188d97a064202fac528e0faf6b5509aec6
kim-elijah-sol <kimelijahsol@gmail.com>
2024-01-12 18:55:26 +0900

    test: [ColorTurn] 게임 승자 로직 테스트 코드

---
+++ src/ColorTurn/logic/get게임_승자.spec.ts
@@ -0,0 +1,85 @@
+import { 바둑알, 바둑칸 } from "../types";
+import { get게임_승자 } from "./get게임_승자";
+
+function create바둑판Mock(바둑알_목록: 바둑알[][]): 바둑칸[][] {
+  return 바둑알_목록.map((바둑알_행) =>
+    바둑알_행.map((바둑알) => ({ 바둑알, is지뢰: false }))
+  );
+}
+
+describe("get게임_승자", () => {
+  it("게임이 종료되었는지 확인한다. : 가로 감지", () => {
+    expect(
+      get게임_승자(
+        create바둑판Mock([
+          ["b", "b", "b", "b", "w", null],
+          [null, null, null, null, null, null],
+          [null, null, null, null, null, null],
+          [null, null, null, null, null, null],
+          [null, null, null, null, null, null],
+          [null, null, null, null, null, null],
+        ])
+      )
+    ).toBe("b");
+  });
+
+  it("게임이 종료되었는지 확인한다. : 세로 감지", () => {
+    expect(
+      get게임_승자(
+        create바둑판Mock([
+          ["b", "w", "b", "b", "w", null],
+          [null, "w", null, null, null, null],
+          [null, "w", null, null, null, null],
+          [null, "w", null, null, null, null],
+          [null, null, null, null, null, null],
+          [null, null, null, null, null, null],
+        ])
+      )
+    ).toBe("w");
+  });
+
+  it("게임이 종료되었는지 확인한다. : 24분면 방향 감지", () => {
+    expect(
+      get게임_승자(
+        create바둑판Mock([
+          ["b", "w", "b", "b", "w", null],
+          [null, "b", null, null, null, null],
+          [null, "w", "b", null, null, null],
+          [null, "w", null, "b", null, null],
+          [null, null, null, null, null, null],
+          [null, null, null, null, null, null],
+        ])
+      )
+    ).toBe("b");
+  });
+
+  it("게임이 종료되었는지 확인한다. : 13분면 방향 감지", () => {
+    expect(
+      get게임_승자(
+        create바둑판Mock([
+          ["b", "w", "b", "b", "w", null],
+          [null, null, null, "w", null, null],
+          [null, "b", "w", null, null, null],
+          [null, "w", null, "b", null, null],
+          [null, null, null, null, null, null],
+          [null, null, null, null, null, null],
+        ])
+      )
+    ).toBe("w");
+  });
+
+  it("게임이 종료되었는지 확인한다. : 미종료", () => {
+    expect(
+      get게임_승자(
+        create바둑판Mock([
+          [null, null, null, null, null, null],
+          [null, "w", null, null, null, null],
+          [null, null, "w", null, null, null],
+          [null, null, null, "w", null, null],
+          [null, "b", "b", "b", null, null],
+          [null, null, null, null, null, null],
+        ])
+      )
+    ).toBe(null);
+  });
+});
