5a4b29e889902286d3c102d1f99cc67622752f58
kim-elijah-sol <kimelijahsol@gmail.com>
2024-01-13 00:40:55 +0900

    feat: [ColorTurn] 바둑판 렌더링

---
+++ src/ColorTurn/components/바둑칸.tsx
@@ -0,0 +1,14 @@
+function 바둑칸() {
+  return (
+    <div
+      style={{
+        width: 100,
+        height: 100,
+        border: "1px solid black",
+        boxSizing: "border-box",
+      }}
+    ></div>
+  );
+}
+
+export default 바둑칸;
--- src/ColorTurn/index.tsx
+++ src/ColorTurn/index.tsx
@@ -1,14 +1,32 @@
-import { useEffect } from "react";
+import 바둑칸 from "./components/바둑칸";
 import useColorTurn from "./hooks/useColorTurn";
 
 function ColorTurn() {
   const { 바둑판 } = useColorTurn();
 
-  useEffect(() => {
-    console.log(바둑판);
-  }, [바둑판]);
-
-  return <></>;
+  return (
+    <main
+      style={{
+        border: "1px solid black",
+        width: 600,
+        height: 600,
+        margin: "auto",
+      }}
+    >
+      {바둑판.map((행, y) => (
+        <div
+          key={y}
+          style={{
+            display: "flex",
+          }}
+        >
+          {행.map((셀, x) => (
+            <바둑칸 key={x} />
+          ))}
+        </div>
+      ))}
+    </main>
+  );
 }
 
 export default ColorTurn;
