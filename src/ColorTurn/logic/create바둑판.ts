5ff60f9f54a6db531ad54e167316bbc5e43853e5
kim-elijah-sol <kimelijahsol@gmail.com>
2024-01-12 18:53:54 +0900

    feat: [ColorTurn] 게임 상수 적용

--- src/ColorTurn/logic/create바둑판.ts
+++ src/ColorTurn/logic/create바둑판.ts
@@ -1,15 +1,14 @@
+import { 바둑판_길이 } from "../constant";
 import { 바둑칸 } from "../types";
 import { get지뢰Indexs } from "./get지뢰Indexs";
 
-const size = 6;
-
 export function create바둑판(): 바둑칸[][] {
   const 바둑판: 바둑칸[][] = [];
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < 바둑판_길이; i++) {
     const 지뢰Indexs = get지뢰Indexs();
 
     바둑판.push([]);
-    for (let j = 0; j < size; j++) {
+    for (let j = 0; j < 바둑판_길이; j++) {
       바둑판[i].push({ 바둑알: null, is지뢰: 지뢰Indexs.includes(j) });
     }
   }
--- src/ColorTurn/logic/get지뢰Indexs.ts
+++ src/ColorTurn/logic/get지뢰Indexs.ts
@@ -1,7 +1,9 @@
+import { 바둑판_길이, 지뢰_개수 } from "../constant";
+
 export function get지뢰Indexs(): number[] {
   const 지뢰Indexs: number[] = [];
-  while (지뢰Indexs.length < 3) {
-    const 지뢰Index = Math.floor(Math.random() * 6);
+  while (지뢰Indexs.length < 지뢰_개수) {
+    const 지뢰Index = Math.floor(Math.random() * 바둑판_길이);
     if (!지뢰Indexs.includes(지뢰Index)) {
       지뢰Indexs.push(지뢰Index);
     }
