fe7a9c83a116d89f4e7e5c9ea95a8101d3e91da8
kim-elijah-sol <kimelijahsol@gmail.com>
2024-01-25 11:22:29 +0900

    env: emotion 설정

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,15 @@
 const CracoAlias = require("craco-alias");
 
 module.exports = {
+  babel: {
+    presets: [
+      [
+        "@babel/preset-react",
+        { runtime: "automatic", importSource: "@emotion/react" },
+      ],
+    ],
+    plugins: ["@emotion/babel-plugin"],
+  },
   plugins: [
     {
       plugin: CracoAlias,
