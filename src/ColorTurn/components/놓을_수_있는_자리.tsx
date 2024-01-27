bacd76d3bb089e81d6864c04a04cd17194935adf
kim-elijah-sol <kimelijahsol@gmail.com>
2024-01-27 17:37:25 +0900

    feat: 놓을 수 있는 자리 > 가변적으로 변경되도록 수정

--- src/ColorTurn/components/놓을_수_있는_자리.tsx
+++ src/ColorTurn/components/놓을_수_있는_자리.tsx
@@ -1,11 +1,45 @@
+import { css } from "@emotion/react";
 import { HTMLAttributes } from "react";
-import style from "./놓을_수_있는_자리.module.css";
+import { 바둑판_길이 } from "../constant";
 
-interface Props
-  extends Omit<HTMLAttributes<HTMLDivElement>, "children" | "className"> {}
+interface Props extends Omit<HTMLAttributes<HTMLDivElement>, "children"> {}
 
 function 놓을_수_있는_자리(props: Props) {
-  return <div className={style.p} {...props} />;
+  const size = `calc(calc(calc(100vw - 32px) / ${바둑판_길이}) * 0.6)`;
+
+  return (
+    <div
+      css={css`
+        width: 100%;
+        height: 100%;
+        cursor: pointer;
+        position: relative;
+
+        &::after {
+          content: "";
+          width: ${size};
+          height: ${size};
+          max-width: 60px;
+          max-height: 60px;
+          border-radius: 50%;
+          position: absolute;
+          left: 50%;
+          top: 50%;
+          transform: translate(-50%, -50%);
+          transition: 0.32s;
+          background-color: #00ff00;
+          opacity: 0.35;
+        }
+
+        &:hover {
+          &::after {
+            opacity: 1;
+          }
+        }
+      `}
+      {...props}
+    />
+  );
 }
 
 export default 놓을_수_있는_자리;
