e11412d8d7931fa3394f51ae9d4900304172aea5
kim-elijah-sol <kimelijahsol@gmail.com>
2024-01-27 17:38:54 +0900

    feat: 놓인 바둑알 > 가변적으로 변경되도록 수정

--- src/ColorTurn/components/놓인_바둑알.tsx
+++ src/ColorTurn/components/놓인_바둑알.tsx
@@ -1,4 +1,5 @@
 import { HTMLAttributes } from "react";
+import { 바둑판_길이 } from "../constant";
 
 interface Props
   extends Omit<HTMLAttributes<HTMLDivElement>, "style" | "children"> {
@@ -12,11 +13,15 @@ function 놓인_바둑알({
   is마지막으로_놓인_자리,
   ...props
 }: Props) {
+  const size = `calc(calc(calc(100vw - 32px) / ${바둑판_길이}) * 0.6)`;
+
   return (
     <div
-      style={{
-        width: 60,
-        height: 60,
+      css={{
+        width: size,
+        height: size,
+        maxWidth: 60,
+        maxHeight: 60,
         borderRadius: "50%",
         backgroundColor: 바둑알 === "w" ? "white" : "black",
         cursor: is마지막으로_놓인_자리 ? "not-allowed" : "pointer",
