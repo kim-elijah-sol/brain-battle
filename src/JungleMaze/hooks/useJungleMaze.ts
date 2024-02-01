894816d6d3505154ccd14538a9627ab004b53cce
kim-elijah-sol <kimelijahsol@gmail.com>
2024-02-01 14:58:26 +0900

    feat: [JungleMaze] 남은 조각 추가

--- src/JungleMaze/hooks/useJungleMaze.ts
+++ src/JungleMaze/hooks/useJungleMaze.ts
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import createJungle from "../logic/createJungle";
+import createRandomStreet from "../logic/createRandomStreet";
 
 function useJungleMaze() {
   const [jungle, setJungle] = useState(createJungle());
 
+  const [restPiece, setRestPiece] = useState(createRandomStreet());
+
   return {
     jungle,
+    restPiece,
   };
 }
 
