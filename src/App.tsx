9343ca774e32a86e56a34ee893f4e7425016469c
kim-elijah-sol <kimelijahsol@gmail.com>
2024-02-03 17:11:28 +0900

    feat: ColorTurn, JungleMaze 선택 가능하도록 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
+import ColorTurn from "./ColorTurn";
 import JungleMaze from "./JungleMaze";
 
 function App() {
+  const [game, setGame] = useState<string>("JungleMaze");
+
   return (
     <>
-      <JungleMaze />
+      <div>
+        <input
+          type="radio"
+          id="ColorTurn"
+          checked={game === "ColorTurn"}
+          onChange={() => setGame("ColorTurn")}
+        />
+        <label htmlFor="ColorTurn">ColorTurn</label>
+        <input
+          type="radio"
+          id="JungleMaze"
+          checked={game === "JungleMaze"}
+          onChange={() => setGame("JungleMaze")}
+        />
+        <label htmlFor="JungleMaze">JungleMaze</label>
+      </div>
+      {game === "ColorTurn" && <ColorTurn />}
+      {game === "JungleMaze" && <JungleMaze />}
     </>
   );
 }
