185e86ca5c8be51b817232283d9176b228ad012c
kim-elijah-sol <kimelijahsol@gmail.com>
2024-02-04 23:05:31 +0900

    feat: [JungleMaze] 현재 위치 기준 이동할 수 있는 경로 탐색

--- src/JungleMaze/logic/getMovablePosition.ts
+++ src/JungleMaze/logic/getMovablePosition.ts
@@ -8,26 +8,74 @@ function getMovablePosition(
   const movablePosition: [number, number][] = [[y, x]];
 
   const visited: boolean[][] = Array.from({ length: jungle길이 }, () =>
-    Array(7).fill(false)
+    Array(jungle길이).fill(false)
   );
 
   visited[y][x] = true;
 
+  let 탐색할_칸: [number, number][] = [[y, x]];
+
   while (true) {
-    /**
-     * 북쪽으로 이동 가능한지 확인
-     */
-    if (jungle[y][x].street.includes("n")) {
-      if (!visited[y - 1][x] && jungle[y - 1][x].street.includes("s")) {
-        movablePosition.push([y - 1, x]);
-        visited[y - 1][x] = true;
-        y -= 1;
-      } else {
-        break;
+    let 다음_탐색할_칸: [number, number][] = [];
+
+    for (const 칸 of 탐색할_칸) {
+      const [y, x] = 칸;
+
+      for (const street of jungle[y][x].street) {
+        const [dy, dx] = (() => {
+          switch (street) {
+            case "n":
+              return [-1, 0];
+            case "s":
+              return [1, 0];
+            case "w":
+              return [0, -1];
+            case "e":
+              return [0, 1];
+          }
+        })();
+
+        const findStreet = (() => {
+          switch (street) {
+            case "n":
+              return "s";
+            case "s":
+              return "n";
+            case "w":
+              return "e";
+            case "e":
+              return "w";
+          }
+        })();
+
+        const [ny, nx] = [y + dy, x + dx];
+
+        // 맵에서 벗어나는 경우
+        if (ny < 0 || ny >= jungle길이 || nx < 0 || nx >= jungle길이) {
+          continue;
+        }
+
+        // 이동할 수 없는 길인 경우
+        if (jungle[ny][nx].street.includes(findStreet) === false) {
+          continue;
+        }
+
+        // 이미 방문한 경우
+        if (visited[ny][nx]) {
+          continue;
+        }
+
+        visited[ny][nx] = true;
+        다음_탐색할_칸.push([ny, nx]);
+        movablePosition.push([ny, nx]);
       }
-    } else {
+    }
+
+    if (다음_탐색할_칸.length === 0) {
       break;
     }
+
+    탐색할_칸 = 다음_탐색할_칸;
   }
 
   return movablePosition;
